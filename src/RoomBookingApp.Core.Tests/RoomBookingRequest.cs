2316d2dd88aa18312c94319f415cc9da2d0f7ad6
StefanStrauberg <stefanstrauberg@gmail.com>
2024-01-01 16:20:39 +0300

    create first test

---
+++ src/RoomBookingApp.Core.Tests/RoomBookingRequest.cs
@@ -0,0 +1,8 @@
+namespace RoomBookingApp.Core.Tests;
+
+internal class RoomBookingRequest
+{
+    public string FullName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+}
---
+++ src/RoomBookingApp.Core.Tests/RoomBookingResult.cs
@@ -0,0 +1,8 @@
+namespace RoomBookingApp.Core.Tests;
+
+internal class RoomBookingResult
+{
+    public string FullName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+}
